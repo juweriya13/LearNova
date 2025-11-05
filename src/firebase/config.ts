b33dc7508cb477199060eee98ef09739ff40aa77
Javeriya Khan <kjaveriya13@gmail.com>
2025-11-05 05:45:56 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2330014822-cf291",
+  "appId": "1:466218317323:web:86a5fb39199611e394790e",
+  "apiKey": "AIzaSyCYfOEkfmOv9ANVY9HvufAHQdHeVr1Oyv8",
+  "authDomain": "studio-2330014822-cf291.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "466218317323"
+};
